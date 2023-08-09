a72a838deaa9de504804bb287f5f53cf9cddac26
Sfrisio <gianluca.ricci@outlook.com>
2023-08-09 20:14:51 +0200

    add fetch repo info

--- go-apt-search.go
+++ go-apt-search.go
@@ -22,6 +22,14 @@ type APTPackages struct {
 	Sha256       string   `json:"SHA256"`
 }
 
+type RepoArchive struct {
+	Domain       string `json:"Domain"`
+	Distribution string `json:"Distribution"`
+	Area         string `json:"Area"`
+	Architecture string `json:"Architecture"`
+	ListFileName string `json:"ListFileName"`
+}
+
 // AptSearch: allows to perform a targeted search using the exact name of the package to be searched,
 //
 // or a keyword search that will result in all packages that include that string in the name
@@ -50,8 +58,64 @@ func AptListAll() ([]APTPackages, error) {
 	if errGetRepoFileList != nil {
 		return nil, errGetRepoFileList
 	}
-	var packagesList []APTPackages
+	allPackagesList, errBuildPackagesList := buildPackagesList(allPackagesFiles)
+	if errBuildPackagesList != nil {
+		return nil, errBuildPackagesList
+	}
+	return allPackagesList, nil
+}
+
+// GetRepoDomain: returns a list of currently active repositories by distribution and area
+func GetRepoDomain() ([]RepoArchive, error) {
+	repoList, errGetRepoFileList := getRepoFileList()
+	if errGetRepoFileList != nil {
+		return nil, errGetRepoFileList
+	}
+	var repoDomainList []RepoArchive
+	for _, repo := range repoList {
+		repoDomain := strings.Split(repo, "_")
+		var extractedDistribution string
+		var extractedArea string
+		for i, repoFields := range repoDomain {
+			if repoFields == "dists" {
+				extractedDistribution = repoDomain[i+1]
+				extractedArea = repoDomain[i+2]
+			}
+		}
+		repoDomainList = append(repoDomainList, RepoArchive{
+			Domain:       repoDomain[0],
+			Distribution: extractedDistribution,
+			Area:         extractedArea,
+			Architecture: repoDomain[len(repoDomain)-2],
+			ListFileName: repo,
+		})
+
+	}
+	return repoDomainList, nil
+}
+
+// getRepoFileList: read files from /var/lib/apt/lists and return only packages
+//
+// I preferred to use os.ReadDir instead of filepath.Walk because I am not interested in the list of files in the partial directory
+func getRepoFileList() ([]string, error) {
+	allPackagesFiles, errReadDir := os.ReadDir(aptListPath)
+	if errReadDir != nil {
+		return nil, errReadDir
+	}
+	var matchingPackagesFiles []string
+	filterPackagesFile, _ := regexp.Compile(`.*\_Packages$`)
 	for _, packagesFile := range allPackagesFiles {
+		if filterPackagesFile.MatchString(packagesFile.Name()) {
+			matchingPackagesFiles = append(matchingPackagesFiles, packagesFile.Name())
+		}
+	}
+	return matchingPackagesFiles, nil
+}
+
+// buildPackagesList: return packages available from a list of repositories
+func buildPackagesList(repoList []string) ([]APTPackages, error) {
+	var packagesList []APTPackages
+	for _, packagesFile := range repoList {
 		readPackageFile, errOpen := os.ReadFile(filepath.Join(aptListPath, packagesFile))
 		if errOpen != nil {
 			return nil, errOpen
@@ -99,22 +163,4 @@ func AptListAll() ([]APTPackages, error) {
 		}
 	}
 	return packagesList, nil
-}
-
-// getRepoFileList: read files from /var/lib/apt/lists and return only packages
-//
-// I preferred to use os.ReadDir instead of filepath.Walk because I am not interested in the list of files in the partial directory
-func getRepoFileList() ([]string, error) {
-	allPackagesFiles, errReadDir := os.ReadDir(aptListPath)
-	if errReadDir != nil {
-		return nil, errReadDir
-	}
-	var matchingPackagesFiles []string
-	filterPackagesFile, _ := regexp.Compile(`.*\_Packages$`)
-	for _, packagesFile := range allPackagesFiles {
-		if filterPackagesFile.MatchString(packagesFile.Name()) {
-			matchingPackagesFiles = append(matchingPackagesFiles, packagesFile.Name())
-		}
-	}
-	return matchingPackagesFiles, nil
 }
